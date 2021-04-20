f7407202e19a3283e884a9e611150e2a4bee9a17
Cian <23337360+CianHub@users.noreply.github.com>
2021-04-20 20:41:49 +0100

    add css modules and styling for EventList and EventItem components

--- components/events/EventItem.js
+++ components/events/EventItem.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import Link from 'next/link';
+import classes from './event-item.module.css';
 
 export const EventItem = ({ event }) => {
   const readableDate = new Date(event.date).toLocaleDateString('en-IE', {
@@ -12,19 +13,20 @@ export const EventItem = ({ event }) => {
   const exploreLink = `/events/${event.id}`;
 
   return (
-    <li>
+    <li className={classes.item}>
       <img src={'/' + event.image} alt={event.title} />
-      <div>
-        <div>
+      <div className={classes.content}>
+        <div className={classes.summary}>
           <h2>{event.title}</h2>
+          <div className={classes.date}>
+            <time>{readableDate}</time>
+          </div>
+          <div className={classes.address}>
+            <address>{formattedAddress}</address>
+          </div>
         </div>
-        <div>
-          <time>{readableDate}</time>
-        </div>
-        <div>
-          <address>{formattedAddress}</address>
-        </div>
-        <div>
+
+        <div className={classes.actions}>
           <Link href={exploreLink}>Explore Event</Link>
         </div>
       </div>
--- components/events/EventList.js
+++ components/events/EventList.js
@@ -1,9 +1,10 @@
 import React from 'react';
 import { EventItem } from './EventItem';
+import classes from './event-list.module.css';
 
 export const EventList = ({ events }) => {
   return (
-    <ul>
+    <ul className={classes.list}>
       {events.map((event) => {
         return <EventItem key={event.id} event={event} />;
       })}
